e6201ae4ab1d7bc7e220a953dffa2969ecbe3db9
Tigerband66 <Brandendavis619@gmail.com>
2024-05-05 02:11:21 -0500

    How old is the Professor?

---
+++ Week 1/main (3).cpp
@@ -0,0 +1,16 @@
+// Branden Davis
+// coding 1 spring 2024
+// week 1
+
+// these are libraries of code that we are calling
+#include <iostream>    // In-out stream, for sending data to and from console
+#include <string>      // string of text, string library
+using namespace std; // so we dont have to type "std::" all the time
+
+
+  int main() {
+  cout << "Hello Branden!\n";
+  int professorsAge = 22;
+  cout << "The professor is " << professorsAge << " years old.\n";
+}
+
